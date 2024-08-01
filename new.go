049f6822239487dd81c15cddd0e689204570a326
MeloSofia <sofia.melo@estudante.ifro.edu.br>
2024-08-01 17:56:01 -0400

    Update

--- new.go
+++ new.go
@@ -7,19 +7,15 @@ func main() {
 	var nome string
 	var a, b int
 
-	// Solicita o nome do usuário
 	fmt.Println("Digite seu nome:") 
 	fmt.Scanln(&nome)
 	fmt.Printf("O nome é: %s\n", nome)
 
-	// Solicita o primeiro número
 	fmt.Println("Digite o primeiro número:")
 	fmt.Scanln(&a)
 
-	// Solicita o segundo número
 	fmt.Println("Digite o segundo número:")
 	fmt.Scanln(&b)
 
-	// Calcula e imprime a soma
 	fmt.Printf("A soma dos números é: %d\n", a+b)
 }
